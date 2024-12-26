c43bed9feb306ec078592861c314924896db9d63
Soham-Debnath <20debnathsoham04@gmail.com>
2024-12-27 00:43:46 +0530

    set_prob

---
+++ 03_set_prob.py
@@ -0,0 +1,23 @@
+# Take 8 input number:  and display all in unique manner (i.e. set).
+
+s= set()
+
+a1=int(input("enter number: "))
+s.add(a1)
+a2=int(input("enter number: "))
+s.add(a2)
+a3=int(input("enter number: "))
+s.add(a3)
+a4=int(input("enter number: "))
+s.add(a4)
+a5=int(input("enter number: "))
+s.add(a5)
+a6=int(input("enter number: "))
+s.add(a6)
+a7=int(input("enter number: "))
+s.add(a7)
+a8=int(input("enter number: "))
+s.add(a8)
+
+print(s)
+      
